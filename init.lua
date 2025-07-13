7056449c124fa431eb69ed1bac3077689b05ec80
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-07-12 23:11:30 -0400

    init.lua: switch signs

--- init.lua
+++ init.lua
@@ -75,8 +75,8 @@ vim.diagnostic.config({
     signs = { text = {
         [vim.diagnostic.severity.ERROR] = "",
         [vim.diagnostic.severity.WARN ] = "",
-        [vim.diagnostic.severity.HINT ] = "",
-        [vim.diagnostic.severity.INFO ] = "",
+        [vim.diagnostic.severity.INFO ] = "",
+        [vim.diagnostic.severity.HINT ] = "",
     },
     severity_sort = true,
 }})
